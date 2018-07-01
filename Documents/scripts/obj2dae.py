bd2573ae45c33aa8746463323ff47d45b3b1bc1d
Kevin Chumbley <kchumbley@gmail.com>
2018-07-01 00:00:52 -0700

    land after finishing flightplan

--- Documents/scripts/obj2dae.py
+++ Documents/scripts/obj2dae.py
@@ -2,9 +2,9 @@
 
 import bpy
 
-infile_loc = '/home/kevin/Documents/my_models/my_model/obj/map.obj'
-textures_loc = '/home/kevin/Documents/OSM2World/build/textures/'
-outfile_loc = '/home/kevin/Documents/my_models/my_model/dae/my_model.dae'
+infile_loc = '/home/kchumbley/Documents/myModels/myModel/obj/map.obj'
+textures_loc = '/home/kchumbley/Documents/OSM2World/textures/'
+outfile_loc = '/home/kchumbley/Documents/myModels/myModel/dae/my_model.dae'
 
 imported_object = bpy.ops.import_scene.obj(filepath=infile_loc)
 obj_object = bpy.context.selected_objects[0]
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Setup and control scripts for thesis simulation IN_PROGRESS
 
 # pull
-<pre>cd ~
+<pre>
+cd ~
 git init
 git remote add origin https://github.com/kchumbley/myhome.git
 git pull origin master
